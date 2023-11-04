81d7b1565c335c3bb564c4791222611335eec38a
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2023-11-04 18:20:46 -0300

    feat: criacao dos elementos principais para a cena. Criacao do objeto de renderizacao (renderer), cena (scene) e camera.

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('SBGames2023')
+import * as THREE from 'three'
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x000000)
+
+const camera = new THREE.PerspectiveCamera(
+  40, //campo de visao vertical 
+  window.innerWidth / window.innerHeight, //aspecto da imagem (Largura/Altura)
+  0.1, //Plano proximo
+  1000 //Plano distante
+);
+camera.position.set(0, .25, 2)
